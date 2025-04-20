927971e66dfa310845c3c0c587ecc2f85130147c
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-04-20 16:21:35 +0200

    Add examples (doc)
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/context/time.rs
+++ src/context/time.rs
@@ -30,15 +30,47 @@ impl QcContext {
         GnssAbsoluteTime::new(&polynomials)
     }
 
-    /// Perform precise transposition of each individual components (input products) contained in current [QcContext]
-    /// to desired [TimeScale]. NB: transposition might not be feasible for some components, therefore
+    /// Precise temporal transposition of each individual products contained in current [QcContext].
+    /// NB: transposition might not be feasible for some components, therefore
     /// you should double check the newly obtained [QcContext].
     ///
     /// This may apply to your [SP3] products, if feature is activated.
     ///
     /// Example (1): precise RINEX transpositions
+    /// ```
+    /// use gnss_qc::prelude::{QcContext, TimeScale};
     ///
-    /// Example (2): RINEX + SP3 PPP transposition
+    /// let mut context = QcContext::new();
+    ///
+    /// // GPST observations
+    /// context.load_gzip_rinex_file("data/CRNX/V3/ESBC00DNK_R_20201770000_01D_30S_MO.crx.gz")
+    ///     .unwrap();
+    ///
+    /// // Transposition attempt
+    /// let transposed = context.timescale_transposition(TimeScale::GST);
+    /// let transposed_obs = transposed.observation().unwrap();
+    ///
+    /// // For this to work, Observations are not enough.
+    /// for t in transposed_obs.epoch_iter() {
+    ///     assert_eq!(t.time_scale, TimeScale::GPST);
+    /// }
+    ///
+    /// // You need to stack NAV RINEX for that day as well
+    /// context.load_gzip_rinex_file("data/NAV/V3/ESBC00DNK_R_20201770000_01D_MN.rnx.gz")
+    ///     .unwrap();
+    ///
+    /// let transposed = context.timescale_transposition(TimeScale::GST);
+    /// let transposed_obs = transposed.observation().unwrap();
+    ///
+    /// // For this to work, Observations are not enough.
+    /// for t in transposed_obs.epoch_iter() {
+    ///     assert_eq!(t.time_scale, TimeScale::GST);
+    /// }
+    /// ```
+    ///
+    /// Example (2): SP3 transposition.
+    /// SP3 are totally valid in any GNSS timescale, you can use this framework
+    /// to reformat as desired !
     ///
     pub fn timescale_transposition(&self, target: TimeScale) -> Self {
         let mut s = self.clone();
