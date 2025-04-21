1148004ef10cde3177afcdc24016ff492aca78bd
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-04-21 14:13:33 +0200

    Progress towards NAV PVT integration
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/context/mod.rs
+++ src/context/mod.rs
@@ -1,6 +1,5 @@
 //! GNSS processing context definition.
 use std::{
-    cell::RefCell,
     collections::HashMap,
     ffi::OsStr,
     path::{Path, PathBuf},
@@ -33,18 +32,6 @@ use crate::{error::Error, prelude::ProductType};
 
 #[cfg(feature = "navigation")]
 use crate::prelude::{Almanac, Frame};
-
-pub(crate) struct QcContextRef<'a> {
-    ctx: RefCell<&'a QcContext>,
-}
-
-impl<'a> QcContextRef<'a> {
-    /// Obtain reference to Broadcast navigation data
-    pub fn brdc_navigation(&self) -> Option<&'a Rinex> {
-        let brdc = self.ctx.borrow().brdc_navigation()?;
-        Some(brdc)
-    }
-}
 
 /// [QcContext] is a general structure capable to store most common GNSS data.   
 /// It is dedicated to post processing workflows, precise timing or atmosphere analysis.
