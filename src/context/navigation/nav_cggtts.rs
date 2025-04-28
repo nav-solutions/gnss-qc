f31883fb4350e482bb46a8bf9339683aea7bf9dc
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-04-28 16:03:20 +0200

    Progress towards integration
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/context/navigation/nav_cggtts.rs
+++ src/context/navigation/nav_cggtts.rs
@@ -1,8 +1,11 @@
 use crate::{context::navigation::NavPPPSolver, prelude::QcContext};
 
-use gnss_rtk::prelude::Config as PVTConfig;
+use gnss_rtk::prelude::{Config as PPPConfig, Error as SolverError, SPEED_OF_LIGHT_M_S};
 
-use cggtts::prelude::SkyTracker;
+use cggtts::prelude::{
+    CommonViewCalendar as CvCalendar, Observation as FitObservation, SkyTracker,
+    Track as CggttsTrack,
+};
 
 /// [NavCggttsSolver] is very similar to [NavPPPSolver] and operates identically.
 ///
@@ -19,21 +22,60 @@ use cggtts::prelude::SkyTracker;
 #[cfg(feature = "navigation")]
 #[cfg_attr(docsrs, doc(cfg(feature = "navigation")))]
 pub struct NavCggttsSolver<'a> {
-    nav_ppp: NavPPPSolver<'a>,
     tracker: SkyTracker,
+    cv_calendar: CvCalendar,
+    nav_ppp: NavPPPSolver<'a>,
 }
 
 #[cfg(feature = "navigation")]
 #[cfg_attr(docsrs, doc(cfg(feature = "navigation")))]
 impl QcContext {
-    /// Obtain a [NavCggttsSolver] from any navigation compatible [QcContext], ready to
-    /// solve [CGGTTS] solutions.
-    pub fn nav_cggtts_solver<'a>(&'a self, cfg: PVTConfig) -> Option<NavCggttsSolver<'a>> {
+    /// Obtain a [NavCggttsSolver] from any navigation compatible [QcContext], that
+    /// you can then iterate through [CggttsTrack]s (solutions).
+    pub fn nav_cggtts_solver<'a>(&'a self, cfg: PPPConfig) -> Option<NavCggttsSolver<'a>> {
         let nav_ppp = self.nav_ppp_solver(cfg)?;
+
+        let cv_calendar = CvCalendar::bipm();
 
         Some(NavCggttsSolver {
             nav_ppp,
+            cv_calendar,
             tracker: SkyTracker::new(),
         })
+    }
+}
+
+impl<'a> Iterator for NavCggttsSolver<'a> {
+    type Item = Result<CggttsTrack, SolverError>;
+
+    /// Iterate [NavPPPSolver] and try to obtain a new [PVTSolution].
+    fn next(&mut self) -> Option<Self::Item> {
+        let pvt = self.nav_ppp.next()?;
+
+        match pvt {
+            Ok(pvt) => {
+                for sv in pvt.sv.iter() {
+                    let refsys = pvt.clock_offset_s;
+                    let refsv = refsys + sv.clock_correction.unwrap_or_default().to_seconds();
+                    let mdtr = sv.tropo_bias.unwrap_or_default() / SPEED_OF_LIGHT_M_S;
+
+                    // tracking
+                    let track = FitObservation {
+                        epoch: pvt.epoch,
+                        refsv,
+                        refsys,
+                        mdtr,
+                        mdio: None,
+                        msio: None,
+                        elevation: sv.elevation,
+                        azimuth: sv.azimuth,
+                    };
+                }
+            }
+            Err(e) => {
+                error!("pvt solver error: {}", e);
+                return Some(Err(e));
+            }
+        }
     }
 }
