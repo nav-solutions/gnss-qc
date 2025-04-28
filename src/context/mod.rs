bc37f399b59c4f9b946920d0f8d9ed1764ff34f0
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-04-28 13:57:17 +0200

    Improve AbsoluteTime framework
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/context/mod.rs
+++ src/context/mod.rs
@@ -9,8 +9,6 @@ use crate::prelude::{Rinex, TimeScale};
 
 use qc_traits::Merge;
 
-pub mod time;
-
 pub(crate) mod blob;
 use blob::BlobData;
 
@@ -25,6 +23,10 @@ mod sp3;
 #[cfg(feature = "navigation")]
 #[cfg_attr(docsrs, doc(cfg(feature = "navigation")))]
 mod navigation;
+
+#[cfg(feature = "navigation")]
+#[cfg_attr(docsrs, doc(cfg(feature = "navigation")))]
+pub mod time;
 
 use qc_traits::{Filter, Preprocessing, Repair, RepairTrait};
 
