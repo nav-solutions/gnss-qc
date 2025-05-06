52b27725077c10c51589df2e855397877abd86d6
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-05-06 12:21:33 +0200

    node attempt
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/pipeline/mod.rs
+++ src/pipeline/mod.rs
@@ -1,272 +1,262 @@
-use crate::{
-    context::{QcContext, QcIndexing},
-    error::QcError,
-    prelude::Epoch,
-    report::QcRunReport,
-    serializer::{
-        data::{QcSerializedItem, QcSerializedSignal, QcSignalData},
-        serializer::QcSerializer,
-    },
-};
+// use crate::{
+//     context::{QcContext, QcIndexing},
+//     error::QcError,
+//     prelude::Epoch,
+//     report::QcRunReport,
+//     serializer::{
+//         data::{QcSerializedItem, QcSerializedSignal, QcSignalData},
+//         serializer::QcSerializer,
+//     },
+// };
 
-use hifitime::Duration;
-use std::sync::{Arc, Mutex};
+// use hifitime::Duration;
+// use std::sync::{Arc, Mutex};
 
-use qc_traits::MaskFilter;
+// use qc_traits::MaskFilter;
 
-use tokio::{
-    sync::{
-        broadcast::{
-            channel as broadcast_channel, error::RecvError, Receiver as BroadcastReceiver,
-            Sender as BroadcastSender,
-        },
-        mpsc::{channel as mpsc_channel, Receiver, Sender},
-        RwLock,
-    },
-    task::spawn_blocking,
-};
+// use tokio::{
+//     sync::{
+//         broadcast::{
+//             channel as broadcast_channel, error::RecvError, Receiver as BroadcastReceiver,
+//             Sender as BroadcastSender,
+//         },
+//         mpsc::{channel as mpsc_channel, Receiver, Sender},
+//         RwLock,
+//     },
+//     task::spawn_blocking,
+// };
 
 use log::debug;
 
-// pub mod node;
+pub mod node;
+
 // pub mod tx_buffer;
 
 // mod tasklet;
 // use tasklet::{observations::QcSignalObservationTask, QcTasklet};
 
-// use flume::Receiver;
+// // use flume::Receiver;
 
-/// Generic [QcPipelineNode] that can receive synchronous data,
-/// process it and stream data out.
-pub trait Node {
-    /// Read a new data point
-    fn read(&mut self);
+// /// [QcObservationsDeserializer] deserializes an observation stream from a unique source!
+// pub struct QcObsExtractor {
+//     /// Source filter
+//     pub source_filter: QcIndexing,
 
-    /// Process data point, producing a result
-    fn process(&mut self);
-}
+//     /// RX handle
+//     pub rx: BroadcastReceiver<QcSerializedItem>,
 
-/// [QcObservationsDeserializer] deserializes an observation stream from a unique source!
-pub struct QcObsExtractor {
-    /// Source filter
-    pub source_filter: QcIndexing,
+//     /// TX handle
+//     pub tx: Sender<QcSerializedSignal>,
+// }
 
-    /// RX handle
-    pub rx: BroadcastReceiver<QcSerializedItem>,
+// impl QcObsExtractor {
+//     pub fn run(&mut self) {
+//         loop {
+//             match self.rx.blocking_recv() {
+//                 Ok(QcSerializedItem::Signal(signal)) => {
+//                     if signal.indexing == self.source_filter {
+//                         match self.tx.blocking_send(signal) {
+//                             Ok(_) => {}
+//                             Err(e) => {
+//                                 error!(
+//                                     "{} (obs-task) - send error {} (=data loss!)",
+//                                     self.source_filter, e
+//                                 );
+//                             }
+//                         }
+//                     }
+//                 }
+//                 Err(RecvError::Closed) => {
+//                     // channel is closed, all points forwarded
+//                     break;
+//                 }
+//                 Err(RecvError::Lagged(lost)) => {
+//                     error!(
+//                         "{} (obs-task) - queue overflow {} messages lost",
+//                         self.source_filter, lost
+//                     );
+//                 }
+//                 _ => {} // filtered out
+//             }
+//         }
+//         debug!("{} (obs-task) - completed", self.source_filter);
+//     }
+// }
 
-    /// TX handle
-    pub tx: Sender<QcSerializedSignal>,
-}
+// pub struct QcRunReporter {
+//     /// Deployment datetime
+//     pub datetime: Epoch,
 
-impl QcObsExtractor {
-    pub fn run(&mut self) {
-        loop {
-            match self.rx.blocking_recv() {
-                Ok(QcSerializedItem::Signal(signal)) => {
-                    if signal.indexing == self.source_filter {
-                        match self.tx.blocking_send(signal) {
-                            Ok(_) => {}
-                            Err(e) => {
-                                error!(
-                                    "{} (obs-task) - send error {} (=data loss!)",
-                                    self.source_filter, e
-                                );
-                            }
-                        }
-                    }
-                }
-                Err(RecvError::Closed) => {
-                    // channel is closed, all points forwarded
-                    break;
-                }
-                Err(RecvError::Lagged(lost)) => {
-                    error!(
-                        "{} (obs-task) - queue overflow {} messages lost",
-                        self.source_filter, lost
-                    );
-                }
-                _ => {} // filtered out
-            }
-        }
-        debug!("{} (obs-task) - completed", self.source_filter);
-    }
-}
+//     /// Path 1 RX handle
+//     pub rx: Receiver<QcSerializedSignal>,
 
-pub struct QcRunReporter {
-    /// Deployment datetime
-    pub datetime: Epoch,
+//     /// Report being redacted
+//     pub report: Arc<Mutex<QcRunReport>>,
+// }
 
-    /// Path 1 RX handle
-    pub rx: Receiver<QcSerializedSignal>,
+// impl QcRunReporter {
+//     pub fn run(&mut self) {
+//         loop {
+//             // retrieve all contributions
+//             match self.rx.blocking_recv() {
+//                 Some(item) => {
+//                     let mut report = self.report.lock().unwrap();
+//                     report.add_signal_contribution(item);
+//                 }
+//                 None => break,
+//             }
+//         }
+//         debug!("run reporting - completed");
+//     }
+// }
 
-    /// Report being redacted
-    pub report: Arc<Mutex<QcRunReport>>,
-}
+// /// [QcPipeline], deployed according to user specs
+// /// and containing several tasklets (=algorithm) to be executed.
+// pub struct QcPipeline<'a> {
+//     /// [QcSerializer] to provide the data stream
+//     serializer: QcSerializer<'a>,
+// }
 
-impl QcRunReporter {
-    pub fn run(&mut self) {
-        loop {
-            // retrieve all contributions
-            match self.rx.blocking_recv() {
-                Some(item) => {
-                    let mut report = self.report.lock().unwrap();
-                    report.add_signal_contribution(item);
-                }
-                None => break,
-            }
-        }
-        debug!("run reporting - completed");
-    }
-}
+// impl QcContext {
+//     /// Form a [QcPipeline], following user specifications (desired tasks and algorithms to deploy),
+//     /// ready to execute. You then have two execution scenarios, depending on your context and ecosystem
+//     /// capabilities:
+//     /// - serial: execute one task after the other by using the proposed [Iterator].
+//     /// The [QcPipeline] job is done when all tasks were performed sequentially.
+//     /// - multithreaded: tasks are performed in parallal, using tokio framework.
+//     pub fn qc_processing_pipeline<'a>(&'a self) -> QcPipeline<'a> {
+//         let serializer = self.serializer();
 
-/// [QcPipeline], deployed according to user specs
-/// and containing several tasklets (=algorithm) to be executed.
-pub struct QcPipeline<'a> {
-    /// [QcSerializer] to provide the data stream
-    serializer: QcSerializer<'a>,
-}
+//         QcPipeline { serializer }
+//     }
+// }
 
-impl QcContext {
-    /// Form a [QcPipeline], following user specifications (desired tasks and algorithms to deploy),
-    /// ready to execute. You then have two execution scenarios, depending on your context and ecosystem
-    /// capabilities:
-    /// - serial: execute one task after the other by using the proposed [Iterator].
-    /// The [QcPipeline] job is done when all tasks were performed sequentially.
-    /// - multithreaded: tasks are performed in parallal, using tokio framework.
-    pub fn qc_processing_pipeline<'a>(&'a self) -> QcPipeline<'a> {
-        let serializer = self.serializer();
+// impl<'a> QcPipeline<'a> {
+//     /// Execute this [QcPipeline] asynchronously, deploying a dedicated tasklet for each job.
+//     /// Otherwise, prefer the serial / synchronous execution, using the proposed [Iterator].
+//     pub async fn run(&mut self) -> Result<QcRunReport, QcError> {
+//         debug!("channels creation..");
 
-        QcPipeline { serializer }
-    }
-}
+//         let (brdc_tx, _) = broadcast_channel(16);
+//         let (path_tx, path_rx) = mpsc_channel(16);
 
-impl<'a> QcPipeline<'a> {
-    /// Execute this [QcPipeline] asynchronously, deploying a dedicated tasklet for each job.
-    /// Otherwise, prefer the serial / synchronous execution, using the proposed [Iterator].
-    pub async fn run(&mut self) -> Result<QcRunReport, QcError> {
-        debug!("channels creation..");
+//         // build
+//         debug!("job definitions..");
+//         let mut pool = Vec::new();
 
-        let (brdc_tx, _) = broadcast_channel(16);
-        let (path_tx, path_rx) = mpsc_channel(16);
+//         let now = Epoch::now().unwrap_or_else(|e| {
+//             error!("failed to report system time: {}", e);
+//             Epoch::default()
+//         });
 
-        // build
-        debug!("job definitions..");
-        let mut pool = Vec::new();
+//         // example: Obs source extractor
+//         let mut obs_task = QcObsExtractor {
+//             tx: path_tx,
+//             rx: brdc_tx.subscribe(),
+//             source_filter: QcIndexing::GeodeticMarker("VLNS0630".to_string()),
+//         };
 
-        let now = Epoch::now().unwrap_or_else(|e| {
-            error!("failed to report system time: {}", e);
-            Epoch::default()
-        });
+//         let mut report = QcRunReport::default();
 
-        // example: Obs source extractor
-        let mut obs_task = QcObsExtractor {
-            tx: path_tx,
-            rx: brdc_tx.subscribe(),
-            source_filter: QcIndexing::GeodeticMarker("VLNS0630".to_string()),
-        };
+//         report.run_summary.num_jobs = 1;
+//         report.run_summary.datetime = now.round(Duration::from_seconds(1.0));
 
-        let mut report = QcRunReport::default();
+//         let report = Arc::new(Mutex::new(report));
 
-        report.run_summary.num_jobs = 1;
-        report.run_summary.datetime = now.round(Duration::from_seconds(1.0));
+//         // Run reporter
+//         let mut reporter = QcRunReporter {
+//             rx: path_rx,
+//             datetime: now,
+//             report: Arc::clone(&report),
+//         };
 
-        let report = Arc::new(Mutex::new(report));
+//         // spawn
+//         debug!("deployment..");
 
-        // Run reporter
-        let mut reporter = QcRunReporter {
-            rx: path_rx,
-            datetime: now,
-            report: Arc::clone(&report),
-        };
+//         pool.push(tokio::task::spawn_blocking(move || {
+//             obs_task.run();
+//         }));
 
-        // spawn
-        debug!("deployment..");
+//         pool.push(tokio::task::spawn_blocking(move || {
+//             reporter.run();
+//         }));
 
-        pool.push(tokio::task::spawn_blocking(move || {
-            obs_task.run();
-        }));
+//         // serialize all data
+//         loop {
+//             match self.serializer.next() {
+//                 Some(data) => match brdc_tx.send(data) {
+//                     Ok(_) => {}
+//                     Err(e) => {
+//                         error!("send failure: {}", e);
+//                         break;
+//                     }
+//                 },
+//                 None => break,
+//             }
+//             tokio::time::sleep(std::time::Duration::from_micros(100)).await;
+//         }
 
-        pool.push(tokio::task::spawn_blocking(move || {
-            reporter.run();
-        }));
+//         // will conclude all tasklets
+//         drop(brdc_tx);
 
-        // serialize all data
-        loop {
-            match self.serializer.next() {
-                Some(data) => match brdc_tx.send(data) {
-                    Ok(_) => {}
-                    Err(e) => {
-                        error!("send failure: {}", e);
-                        break;
-                    }
-                },
-                None => break,
-            }
+//         for thread in pool {
+//             thread.await.unwrap();
+//         }
 
-            tokio::time::sleep(std::time::Duration::from_micros(100)).await;
-        }
+//         let mut report = Arc::try_unwrap(report)
+//             .unwrap_or_else(|_| {
+//                 panic!("failed to obtained redacted report!");
+//             })
+//             .into_inner()
+//             .unwrap_or_else(|e| {
+//                 panic!("failed to obtain redacted report! {}", e);
+//             });
 
-        // will conclude all tasklets
-        drop(brdc_tx);
+//         let now = Epoch::now().unwrap_or_else(|e| {
+//             error!("failed to report system time: {}", e);
+//             Epoch::default()
+//         });
 
-        for thread in pool {
-            thread.await.unwrap();
-        }
+//         report.run_summary.run_duration = now - report.run_summary.datetime;
 
-        let mut report = Arc::try_unwrap(report)
-            .unwrap_or_else(|_| {
-                panic!("failed to obtained redacted report!");
-            })
-            .into_inner()
-            .unwrap_or_else(|e| {
-                panic!("failed to obtain redacted report! {}", e);
-            });
+//         info!("pipeline executed sucessfully!");
+//         Ok(report)
+//     }
+// }
 
-        let now = Epoch::now().unwrap_or_else(|e| {
-            error!("failed to report system time: {}", e);
-            Epoch::default()
-        });
+// #[cfg(test)]
+// mod test {
 
-        report.run_summary.run_duration = now - report.run_summary.datetime;
+//     use std::fs::File;
+//     use std::io::Write;
 
-        info!("pipeline executed sucessfully!");
-        Ok(report)
-    }
-}
+//     use crate::{prelude::QcContext, tests::init_logger};
 
-#[cfg(test)]
-mod test {
+//     #[tokio::test]
+//     async fn basic_pipeline() {
+//         init_logger();
 
-    use std::fs::File;
-    use std::io::Write;
+//         let mut ctx = QcContext::new();
 
-    use crate::{prelude::QcContext, tests::init_logger};
+//         // // load data
+//         // ctx.load_gzip_rinex_file("data/NAV/V3/ESBC00DNK_R_20201770000_01D_MN.rnx.gz")
+//         //     .unwrap();
 
-    #[tokio::test]
-    async fn basic_pipeline() {
-        init_logger();
+//         ctx.load_rinex_file("data/OBS/V3/VLNS0010.22O").unwrap();
+//         ctx.load_rinex_file("data/OBS/V3/VLNS0630.22O").unwrap();
 
-        let mut ctx = QcContext::new();
+//         let mut pipeline = ctx.qc_processing_pipeline();
 
-        // // load data
-        // ctx.load_gzip_rinex_file("data/NAV/V3/ESBC00DNK_R_20201770000_01D_MN.rnx.gz")
-        //     .unwrap();
+//         let report = pipeline
+//             .run()
+//             .await
+//             .unwrap_or_else(|e| panic!("Report synthesis failed with: {}", e));
 
-        ctx.load_rinex_file("data/OBS/V3/VLNS0010.22O").unwrap();
-        ctx.load_rinex_file("data/OBS/V3/VLNS0630.22O").unwrap();
+//         let html = report.render_html().into_string();
 
-        let mut pipeline = ctx.qc_processing_pipeline();
+//         let mut fd = File::create("report.html")
+//             .unwrap_or_else(|e| panic!("Failed to create report.html test file: {}", e));
 
-        let report = pipeline
-            .run()
-            .await
-            .unwrap_or_else(|e| panic!("Report synthesis failed with: {}", e));
-
-        let html = report.render_html().into_string();
-
-        let mut fd = File::create("report.html")
-            .unwrap_or_else(|e| panic!("Failed to create report.html test file: {}", e));
-
-        write!(fd, "{}", html).unwrap_or_else(|e| panic!("Failed to dump HTML: {}", e));
-    }
-}
+//         write!(fd, "{}", html).unwrap_or_else(|e| panic!("Failed to dump HTML: {}", e));
+//     }
+// }
--- src/pipeline/node.rs
+++ src/pipeline/node.rs
@@ -1,3 +1,156 @@
-pub struct Node {
-    
+use log::error;
+
+use crossbeam_channel::{Receiver, RecvError, Sender};
+
+pub trait Job {
+    /// [Job::Input] type
+    type Input;
+
+    /// [Job::Output] type
+    type Output;
+
+    /// Process number of available [Job::Input] symbols, possibly returning an [Job::Output]
+    fn process(&mut self, size: usize, samples: &[Self::Input]) -> Option<Self::Output>;
+}
+
+pub struct Node<I: Clone, O: Clone, J: Job<Input = I, Output = O>> {
+    job: J,
+    name: String,
+    tx: Sender<O>,
+    rx: Receiver<I>,
+    rx_fifo: Vec<I>,
+    rx_size: usize,
+    rx_capacity: usize,
+}
+
+impl<I: Clone, O: Clone, J: Job<Input = I, Output = O>> Node<I, O, J> {
+    /// Create a new [Node]
+    pub fn new(name: &str, rx: Receiver<I>, rx_capacity: usize, tx: Sender<O>, job: J) -> Self {
+        Self {
+            job,
+            tx,
+            rx,
+            rx_size: 0,
+            rx_capacity,
+            name: name.to_string(),
+            rx_fifo: Vec::with_capacity(rx_capacity),
+        }
+    }
+
+    /// Execute this task
+    pub fn run(&mut self) {
+        loop {
+            match self.rx.recv() {
+                Ok(value) => {
+                    // will increase as long as processing does not consume
+                    // maybe we should introduce a max limit
+                    self.rx_fifo.push(value);
+                    self.rx_size += 1;
+                }
+                Err(_) => {
+                    // a message could not be received because the channel is disconnected.
+                    break;
+                }
+            }
+
+            // will not propose until initial capacity is reached
+            if self.rx_size >= self.rx_capacity {
+                if let Some(output) = self.job.process(self.rx_size, self.rx_fifo.as_slice()) {
+                    // post results
+                    match self.tx.send(output) {
+                        Ok(_) => {}
+                        Err(e) => {
+                            error!("{} - failed to propagate data: {}", self.name, e);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use crate::tests::init_logger;
+
+    use super::{Job, Node};
+    use crossbeam_channel::{Receiver, Sender};
+    use threadpool::ThreadPool;
+
+    struct Job1 {}
+
+    impl Job for Job1 {
+        type Input = f64;
+        type Output = f64;
+
+        fn process(&mut self, size: usize, _: &[Self::Input]) -> Option<Self::Output> {
+            if size > 10 {
+                Some(1.0)
+            } else {
+                None
+            }
+        }
+    }
+
+    struct Job2 {}
+
+    impl Job for Job2 {
+        type Input = f64;
+        type Output = u64;
+
+        fn process(&mut self, size: usize, _: &[Self::Input]) -> Option<Self::Output> {
+            if size > 10 {
+                Some(2)
+            } else {
+                None
+            }
+        }
+    }
+
+    #[test]
+    fn node_test() {
+        init_logger();
+
+        // let n_workers = 4;
+        // let mut pool = ThreadPool::new(n_workers);
+
+        let (tx, node1_rx) = crossbeam_channel::unbounded();
+        let (node1_tx, node2_rx) = crossbeam_channel::unbounded();
+        let (node2_tx, output_rx) = crossbeam_channel::unbounded();
+
+        let job1 = Job1 {};
+        let mut node_1 = Node::new("node1", node1_rx, 128, node1_tx, job1);
+
+        let job2 = Job2 {};
+        let mut node_2 = Node::new("node2", node2_rx, 128, node2_tx, job2);
+
+        let task_1 = std::thread::spawn(move || {
+            node_1.run();
+            println!("node_1: is done");
+        });
+
+        let task_2 = std::thread::spawn(move || {
+            node_2.run();
+            println!("node_2: is done");
+        });
+
+        let task_3 = std::thread::spawn(move || loop {
+            match output_rx.recv() {
+                Ok(value) => println!("pipeline output: {}", value),
+                Err(_) => break,
+            }
+        });
+
+        // simule data (input)
+        for i in 0..1024 {
+            let _ = tx.send(i as f64).unwrap();
+        }
+
+        drop(tx);
+
+        task_1.join().unwrap();
+        task_2.join().unwrap();
+        task_3.join().unwrap();
+    }
 }
